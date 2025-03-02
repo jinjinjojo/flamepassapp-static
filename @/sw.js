00e7034eb1a12aaab57615bfa741a9491d6465f6
George <69770151+jinjinjojo@users.noreply.github.com>
2025-03-02 15:33:57 -0800

    add static ultraviolet

--- @/sw.js
+++ @/sw.js
@@ -1,7 +1,21 @@
-importScripts('/@/uv.bundle.js');
-importScripts('/@/uv.config.js');
-importScripts(__uv$config.sw || '/@/uv.sw.js');
+var cacheName = 'TIWcog';
+var filesToCache = [
+  '/js/sw.js'
+];
 
-const sw = new UVServiceWorker();
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener('fetch', event => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
---
+++ @/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/@/0c2f1ddc-5748-436f-ae7c-89b5f708d90e/',
+    bare: 'https://sellix-key-checker-2sfjgz2adq-uc.a.run.app/0acc41f5-6f26-4480-ac31-3f72706a2fc8/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/@/uv/uv.handler.js',
+    bundle: '/@/uv/uv.bundle.js',
+    config: '/@/uv/uv.config.js',
+    sw: '/@/uv/uv.sw.js',
+};
